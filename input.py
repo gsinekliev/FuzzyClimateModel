554be29c2f36b79e17eeb5b4aea74ff50b798169
Georgi Sinekliev <gsinekliev@gmail.com>
2015-02-08 15:23:52 +0200

    Added basic input data utitlities.

--- input.py
+++ input.py
@@ -22,3 +22,22 @@ class InputDataUtilities(object):
         with open( os.getcwd() + '\\Data\\' + fileName, "ab" ) as fObj:
             for row in array_list:
                 np.save( fObj, row )
+
+
+""" There is percepitation info for these cities after July 2012"""
+perc_cities = ['Aalborg', 'Alicante', 'Amsterdam', 'Ankara', 'Antalya', 'Arad', 'Atlanta', 'Baltimore',
+               'Barcelona', 'Basel', 'Beijing', 'Beirut', 'Belfast', 'Belgrade', 'Bergen', 'Berlin', 'Bogota', 'Boston',
+               'Brasilia', 'Bremen', 'Brisbane', 'Bristol', 'Brno', 'Brussels', 'Bucharest', 'Burgas', 'Cairo', 'Charlotte',
+               'Chicago', 'Copenhagen', 'Corfu', 'Craiova', 'Dalian', 'Dallas', 'Debrecen', 'Denver',
+               'Diyarbakir', 'Dongguan', 'Dubai', 'Dublin', 'Dusseldorf', 'Eindhoven', 'Fort Lauderdale', 'Frankfurt',
+               'Geneva', 'Graz', 'Guangzhou', 'Hahn', 'Hamburg', 'Hangzhou', 'Hannover', 'Hanover', 'Helsinki',
+               'Ho Chi Minh City', 'Hong Kong', 'Honolulu', 'Houston', 'Iasi', 'Incheon', 'Istanbul', 'Karachi', 'Kiev',
+               'Kinshasa', 'Klagenfurt', 'Kosice', 'Krakow', 'Kristiansand', 'Kuala Lumpur', 'Kunming', 'Lagos', 'Las Vegas',
+               'Leeds', 'Liege', 'London', 'Los Angeles', 'Luqa', 'Luxembourg', 'Madrid', 'Malaga', 'Malmo', 'Melbourne',
+               'Mexico City', 'Miami', 'Milan', 'Minneapolis', 'Mulhouse', 'Mumbai', 'Munich', 'Natal', 'New York',
+               'Newcastle', 'Nice', 'Orlando', 'Oslo', 'Palma De Mallorca', 'Palma de Mallorca', 'Philadelphia', 'Phoenix',
+               'Pisa', 'Prague', 'Riga', 'Riyadh', 'Salt Lake City', 'Salvador', 'Salzburg', 'San Diego', 'San Francisco',
+               'Sao Paulo', 'Seattle', 'Shenzhen', 'Singapore', 'Skopje', 'Sofia', 'Split', 'Stuttgart', 'Sydney',
+               'Tampa', 'Tehran', 'Thessaloniki', 'Toronto', 'Toulouse', 'Trondheim', 'Ufa', 'Valencia', 'Vancouver', 'Varna',
+               'Verona', 'Vienna', 'Warsaw', 'Washington', 'Wroclaw', 'Xiamen', 'Yekaterinburg', 'Zaragoza', 'Zurich']
+
--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import os
 
 
 def main():
-    # data = inp.InputDataUtilities.getNumpyArrayListFromFile( os.getcwd() + '\\Data\\data_norm_2012_2013.npy' )
-    data = inp.InputDataUtilities.getNumpyArrayListFromFile( os.getcwd() + '\\Data\\data_temp_2012_2013.npy' )
+    #data = inp.InputDataUtilities.getNumpyArrayListFromFile( os.getcwd() + '\\Data\\data_norm_2012_2013.npy' )
+    #data = inp.InputDataUtilities.getNumpyArrayListFromFile( os.getcwd() + '\\Data\\data_temp_2012_2013.npy' )
+    data = inp.InputDataUtilities.getNumpyArrayListFromFile( os.getcwd() + '\\Data\\data_january_2012_2013.npy' )
 
-    print data;
+    training_data =[ t.flatten() for t in data ]
 
+    named_training_data = zip(inp.perc_cities, training_data)
+
+    print named_training_data
 
 if __name__ == '__main__':
     main()
