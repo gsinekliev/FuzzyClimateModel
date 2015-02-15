84f9335e7d6f52020de741db935546a072940416
dpaskov <dpaskov91@gmail.com>
2015-02-15 12:39:17 +0200

    Adding Compositiona Rule. Small edit of rule confidence level.

--- rules/Rule.py
+++ rules/Rule.py
@@ -1,11 +1,13 @@
-from rules.fuzzy_sets import FuzzyTriangle
-
 __author__ = 'Admin'
 
-class Antecedent:
-    def __init(self, fuzzyInput, label):
-        self.fuzzyInput = fuzzyInput
+from rules.fuzzy_sets import FuzzyTriangle
+
+
+class Antecedent(object):
+    def __init__(self, fuzzy_input, label):
+        self.fuzzy_input = fuzzy_input
         self.label = label
+
 '''
 Represents fuzzy rule
 '''
@@ -18,22 +20,36 @@ class Rule(object):
     '''
     Antecedent is fuzzy set for
     '''
-    def add_antecedent(self, antecedent ):
+    def add_antecedent(self, antecedent):
         self._if_part.append(antecedent)
 
     def add_consequent(self, cluster_index):
         self._then_part = cluster_index
 
-    def rule_confidence_level(self):
-        return self._and_operation([antecedent.get_membership_degree for antecedent in self._if_part])
+    def rule_confidence_level(self, input_vector):
+        return self._and_operation( [ antecedent.fuzzy_input.get_membership_degree( input_vector[ ind ] )
+                                      for ind, antecedent in enumerate( self._if_part ) ] )
 
 class RuleGenerator(object):
     def __init__(self):
         pass
 
     def generate_rule(self, synop_objects, membership_matrix, cluster_index):
+        """
+            for one cluster passing synop_objects, membership_matrix for them and cluster_index
+        """
         rule = Rule()
         # TODO: make rules by adding a fuzzy set for each attribute of synop_object and using cluster_index as consquent
         #for synop, membership_vector in zip( synop_objects, membership_matrix ):
         #    for attribute in synop.attributes_dict():
         #        rule.add_antecedent( Antecedent( FuzzyTriangle.from_cluster_data(  ) ),  )
+
+class CompositionRule( object ):
+    def __init__( self, cluster_rules=[] ):
+        self.cluster_rules = cluster_rules
+
+    def add_cluster_rule( self, rule ):
+        self.cluster_rules.append( rule )
+
+    def conclusion_vector( self, input_vector ):
+        return [ cluster_rule.rule_confidence_level( input_vector ) for cluster_rule in self.cluster_rules ]
